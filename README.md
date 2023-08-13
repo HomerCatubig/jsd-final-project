cb34a36e72bf7dba9c9b2752a55c129fcf84683c
Homer Catubig <homer.catubig@gmail.com>
2023-08-13 23:49:17 +1000

    Updated layout

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Final Project
 
 Stay tuned!
+
+## Progress Report
+**2023-08-12** Created a landing page
--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
 console.log('Hello from final project!', axios );
+
+const PLAYER_BASE_URL = 'https://www.balldontlie.io/api/v1/players/';
+const TEAM_BASE_URL = 'https://www.balldontlie.io/api/v1/teams/';
+
+const searchPlayerContainer = document.querySelector('#searchPResults');
+const searchTeamContainer = document.querySelector('#searchTResults');
