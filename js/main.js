a6b5125c3199182689bbfb48717284da5d271ac2
Homer Catubig <homer.catubig@gmail.com>
2023-08-15 11:02:36 +1000

    With API connection

--- js/main.js
+++ js/main.js
@@ -1,7 +1,54 @@
 console.log('Hello from final project!', axios );
 
 const PLAYER_BASE_URL = 'https://www.balldontlie.io/api/v1/players/';
-const TEAM_BASE_URL = 'https://www.balldontlie.io/api/v1/teams/';
 
 const searchPlayerContainer = document.querySelector('#searchPResults');
-const searchTeamContainer = document.querySelector('#searchTResults');
+const searchPlayerNode = document.querySelector('#searchPForm');
+const playerSearchInput = document.querySelector('#searchPlayer');
+
+searchPlayerNode.addEventListener('submit', ev => {
+    console.log('Form submitted!');
+    ev.preventDefault();
+
+    searchPlayerContainer.replaceChildren();
+    loadSearchResults(playerSearchInput.value);
+    
+});
+
+const loadSearchResults = (searchPlayer) => {
+    searchPlayerContainer.replaceChildren();
+
+    axios.get(PLAYER_BASE_URL, {
+        params: {
+            search: searchPlayer,
+        }
+    })
+    .then(res => {
+        searchPlayerContainer.replaceChildren();
+        console.log('Response:', res.data);
+
+        res.data.results.forEach(player => {
+            const divTag = document.createElement('div');
+            divTag.innerHTML = `
+            <h2>${player.first_name} ${player.last_name}</h2>
+            `;
+
+            divTag.className = 'result'
+
+            const resultsParent = document.querySelector('#searchPResults');
+            document.body.appendChild(divTag);
+//             <div class="card">
+//   <img src="img_avatar.png" alt="Avatar" style="width:100%">
+//   <div class="container">
+//     <h4><b>John Doe</b></h4> 
+//     <p>Architect & Engineer</p> 
+//   </div>
+// </div>
+            
+        });
+
+    })
+    .catch()
+}
+
+
