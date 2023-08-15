9493dbdcc01c452b7636a0a6f130f36051008370
Homer Catubig <homer.catubig@gmail.com>
2023-08-15 20:00:10 +1000

    Fixed output & added a trading card template

--- js/main.js
+++ js/main.js
@@ -6,17 +6,18 @@ const searchPlayerContainer = document.querySelector('#searchPResults');
 const searchPlayerNode = document.querySelector('#searchPForm');
 const playerSearchInput = document.querySelector('#searchPlayer');
 
+// Perform a search when the user submits a form
 searchPlayerNode.addEventListener('submit', ev => {
     console.log('Form submitted!');
-    ev.preventDefault();
+    console.log(playerSearchInput.value);
+    ev.preventDefault(); // Stop the form submit from reloading the page
 
     searchPlayerContainer.replaceChildren();
-    loadSearchResults(playerSearchInput.value);
-    
+    loadSearchResults(playerSearchInput.value); // Give the user's input text to AJAX function
 });
 
 const loadSearchResults = (searchPlayer) => {
-    searchPlayerContainer.replaceChildren();
+    searchPlayerContainer.replaceChildren(); // Clear previous search results
 
     axios.get(PLAYER_BASE_URL, {
         params: {
@@ -24,31 +25,27 @@ const loadSearchResults = (searchPlayer) => {
         }
     })
     .then(res => {
-        searchPlayerContainer.replaceChildren();
-        console.log('Response:', res.data);
-
-        res.data.results.forEach(player => {
+        // searchPlayerContainer.replaceChildren();
+        // console.log('Response:', res.data);
+        console.log(res);
+        res.data.data.forEach(player => {
             const divTag = document.createElement('div');
             divTag.innerHTML = `
-            <h2>${player.first_name} ${player.last_name}</h2>
+            <img class="card" src="images/2kCard.png" id=logo alt="Player Card" />
+            <h2 class="firstName">${player.first_name}</h2>
+            <h2 class="lastName">${player.last_name}</h2>
+            <p>${player.position}</p>
             `;
 
-            divTag.className = 'result'
+            divTag.className = 'playerRes';
 
             const resultsParent = document.querySelector('#searchPResults');
-            document.body.appendChild(divTag);
-//             <div class="card">
-//   <img src="img_avatar.png" alt="Avatar" style="width:100%">
-//   <div class="container">
-//     <h4><b>John Doe</b></h4> 
-//     <p>Architect & Engineer</p> 
-//   </div>
-// </div>
-            
+            document.body.appendChild(divTag); 
         });
-
     })
-    .catch()
+    .catch(err => {
+        console.warn('Error loading search results:', err);
+    });
 }
 
-
+// "ternary" operator
