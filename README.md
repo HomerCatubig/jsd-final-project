ba531456b8f9b7260b7e9d41d683be18fbf905b2
Homer Catubig <homer.catubig@gmail.com>
2023-08-17 17:36:17 +1000

    final version

--- README.md
+++ README.md
@@ -3,4 +3,16 @@
 Stay tuned!
 
 ## Progress Report
-**2023-08-12** Created a landing page
+
+## Necessary Deliverables
+A production-ready web application hosted live on the web.
+A public GitHub repository where the codebase is maintained.
+A README.md file in your GitHub repository that includes:
+A link to the live app so people can try it out!
+An overview of your idea, with a description of the key features
+Any wireframes or diagrams you used during the planning stage
+A screenshot or two of the app is nice to have
+Explain a technical hurdle (something you struggled with)
+Explain some things you learned (something you enjoyed)
+If you used technology that we haven't covered in class, provide an overview of that
+Where next? What will you add? (i.e. Wishlist / Future Features)
--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-console.log('Hello from final project!', axios );
+console.log('Hello from final project!', axios);
 
 /* ----- Global Variables ----------- */
 const PLAYER_BASE_URL = 'https://www.balldontlie.io/api/v1/players/';
-const TEAM_BASE_URL = 'https://www.balldontlie.io/api/v1/teams/';
+const TEAM_BASE_URL = 'https://www.balldontlie.io/api/v1/teams';
 // --- PLAYERS --- //
 const searchPlayerContainer = document.querySelector('#searchPResults');
 const searchPlayerNode = document.querySelector('#searchPForm');
@@ -14,6 +14,7 @@ const allTeamsNode = document.querySelector('#allTeams');
 const teamSearchNode = document.querySelector('#searchTForm');
 const teamSearchInput = document.querySelector('#searchTeam');
 const resultsTeamParent = document.querySelector('#searchTResults');
+const spanTeam = document.querySelector("span");
 
 /* ---------------------------------------------- */
 
@@ -35,31 +36,31 @@ const loadSearchResults = (searchPlayer) => {
             search: searchPlayer,
         }
     })
-    .then(res => {
-        console.log('Response:', res.data);
-        // console.log(res);
+        .then(res => {
+            console.log('Response:', res.data);
+            // console.log(res);
 
-        // Generate DIV for each output
-        res.data.data.forEach(player => {
-            const divTag = document.createElement('div');
-            divTag.innerHTML = `
+            // Generate DIV for each output
+            res.data.data.forEach(player => {
+                const divTag = document.createElement('div');
+                divTag.innerHTML = `
             <img class="card" src="images/2kCard.jpg" id=logo alt="Player Card" />
             <h2 class="firstName">${player.first_name}</h2>
             <h2 class="lastName">${player.last_name}</h2>
             <p class="stats"><b style="color:red">Player information:</b></p>
-            <p class="stats"><b>Position:</b> ${player.position === '' ? "-" : player.position }</p>
+            <p class="stats"><b>Position:</b> ${player.position === '' ? "-" : player.position}</p>
             <p class="stats"><b>Height:</b> ${player.height_feet === null ? "-" : player.height_feet} ft ${player.height_inches === null ? "-" : player.height_inches} in</p>
-            <p class="stats"><b>Team:</b> ${player.team.full_name}</p>
+            <p class="stats"><b>Team:</b> <span data-team-id=${player.team.id}>${player.team.full_name}</span></p>
             `;
 
-            divTag.className = 'playerRes';
+                divTag.className = 'playerRes';
 
-            resultsParent.appendChild(divTag); 
+                resultsParent.appendChild(divTag);
+            });
+        })
+        .catch(err => {
+            console.warn('Error loading search results:', err);
         });
-    })
-    .catch(err => {
-        console.warn('Error loading search results:', err);
-    });
 }
 
 /* --- TEAMS --- */
@@ -74,19 +75,58 @@ teamSearchNode.addEventListener('submit', ev1 => {
     allTeamsResults(teamSearchInput.value); // Give the user's input text to AJAX function
 });
 
-const allTeamsResults = (team) => {
+const allTeamsResults = (teamName) => {
     axios.get(TEAM_BASE_URL, {
         params: {
-            search: team,
-            
+            search: teamName,
         }
     })
-    .then(res1 => {
-        console.log('Response:', res1.data);
+        .then(res1 => {
+            console.log('Response:', res1.data);
 
 
-        // Generate DIV for each output
-        res1.data.data.forEach(team => {
+            // Generate DIV for each output
+            res1.data.data.forEach(team => {
+                const divTag = document.createElement('div');
+                divTag.innerHTML = `
+            <img class="card" src="images/2kCard.jpg" id=logo alt="Team Card" />
+            <p class="team"><b></b> ${team.name}</p>
+            <p class="teamFN"><b>${team.full_name}</b></p>
+            <p class="teamInfo"><b>City:</b> ${team.city}</p>
+            <p class="teamInfo"><b>Conference:</b> ${team.conference}</p>
+            <p class="teamInfo"><b>Division:</b> ${team.division}</p>
+            <p class="abrv"><b>${team.abbreviation}</b></p>
+            `;
+
+                divTag.className = 'teamRes';
+
+                resultsTeamParent.appendChild(divTag);
+            });
+        })
+        .catch(err => {
+            console.warn('Error loading search results:', err);
+        });
+}
+
+// Load ALL teams when the "TEAMS" link is clicked
+allTeamsNode.addEventListener('click', ev2 => {
+    console.log('Form submitted!');
+    console.log(teamSearchInput.value);
+    ev2.preventDefault(); // Stop the form submit from reloading the page
+
+    resultsTeamParent.replaceChildren();
+    allTeamsResults(teamSearchInput.value); // Give the user's input text to AJAX function
+});
+
+const teamDetails = (teamID) => {
+    // console.log(TEAM_BASE_URL + "/" + teamID);
+    resultsTeamParent.replaceChildren();
+    axios.get(`${TEAM_BASE_URL}/${teamID}`)
+        .then(res => {
+            console.log(res);
+
+            let team = res.data;
+            // console.log(team);
             const divTag = document.createElement('div');
             divTag.innerHTML = `
             <img class="card" src="images/2kCard.jpg" id=logo alt="Team Card" />
@@ -100,20 +140,16 @@ const allTeamsResults = (team) => {
 
             divTag.className = 'teamRes';
 
-            resultsTeamParent.appendChild(divTag); 
+            resultsTeamParent.appendChild(divTag);
+        })
+        .catch(err => {
+            console.warn('Error loading search results:', err);
         });
-    })
-    .catch(err => {
-        console.warn('Error loading search results:', err);
-    });
+    console.log(teamID);
 }
 
-// Load ALL teams when the "TEAMS" link is clicked
-allTeamsNode.addEventListener('click', ev2 => {
-    console.log('Form submitted!');
-    console.log(teamSearchInput.value);
-    ev2.preventDefault(); // Stop the form submit from reloading the page
-
-    resultsTeamParent.replaceChildren();
-    allTeamsResults(teamSearchInput.value); // Give the user's input text to AJAX function
+resultsParent.addEventListener('click', ev1 => {
+    let teamIdNum = Number(ev1.target.closest("span").dataset.teamId);
+    // console.log(teamIdNum);
+    teamDetails(teamIdNum);
 });
